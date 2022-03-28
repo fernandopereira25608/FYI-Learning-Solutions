5cfe61fbeb12e489ca2b7ec288075d18c5506d1c
Fernando <fernandospereira27@gmail.com>
2022-03-27 23:12:51 -0300

    feat:#Fernando- add InforEmpresa - Contato

---
+++ ReactJS/fyi-solutions/src/Component/Contato/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./style.css";
+import IconPessoa from "../../Images/IconPessoa.svg";
+import IconTelefone from "../../Images/IconTelefone.svg";
+
+const Contato = () => {
+  return (
+    <>
+      <div class="contato">
+        <h1 class="titulo-contato">Fale Conosco</h1>
+        <form class="form-contato">
+          <label class="label-form">
+            <input type="text" placeholder="Nome" />
+          </label>
+
+          <label class="label-form">
+            <input type="text" placeholder="Email" />
+            <span>
+              <img src={IconPessoa}></img>
+            </span>
+          </label>
+
+          <label class="label-form">
+            <input type="text" placeholder="Telefone" />
+            <span>
+              <img src={IconTelefone}></img>
+            </span>
+          </label>
+
+          <textarea class="form-mensagem" placeholder="Mensagem" > </textarea> 
+
+
+          <div class="form-content-checkbox">
+            <label>
+              <input type="radio" />
+              <span>Deseja receber informações e promoções da FYI?</span>
+            </label>
+          
+            <label>
+              <input type="radio" />
+              <span>
+                Informo que eu Li e aceito a politica de provacidade da FYI?
+              </span>
+            </label>
+        </div>
+
+        <button class="button-form">
+            ENVIAR
+        </button>
+       
+
+
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Contato;
--- ReactJS/fyi-solutions/src/Pages/Inicial/App.js
+++ ReactJS/fyi-solutions/src/Pages/Inicial/App.js
@@ -1,32 +1,45 @@
-import banner from '../../Images/banner.svg';
-import logo from '../../Images/Logo.svg';
-import './App.css';
+import logo from "../../Images/Logo.svg";
+import "./App.css";
+import InfoEmpresa from "../../Component/InfoEmpresa/index.js";
+import Contato from "../../Component/Contato";
 
 function Inicial() {
   return (
     <div>
-    <div className="header">
-      <img src={logo} class="logo-inicial"/>
-        <div class="header-content">
-          <div class="header-links">
-            <a>Cursos</a>
-            <a>Sobre</a>
-            <a>Contato</a>
-          </div>
-          <button type="button" class="login-button">Login</button>
-        </div>
-    </div>
-    <main>
-      <div class="image-content">
-        <img src={banner} class="banner"/>
-      </div>
-      <div class="frase">
-        <p>DESDE 2009 O PRINCIPAL CENTRO DE TREINAMENTOS OFICIAL PARA MICROSOFTS DYNAMICS 365</p>
-      </div>
-      <div class="info-empresa">
+      <header className="header">
+        <nav class="header-nav">
+          <img src={logo} class="logo-inicial" />
+          <div class="header-content">
+            <div class="header-links">
+              <a>Cursos</a>
+              <a>Sobre</a>
+              <a>Contato</a>
+            </div>
 
-      </div>
-    </main>
+            <button type="button" class="login-button">
+              Login
+            </button>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <figure class="image-content">
+          <div class="frase">
+            <h1>Sobre a Empresa</h1>
+            <h2>SOLUÇÕES DE APRENDIZAGEM</h2>
+
+            <p>
+              Desde 2009 somos o principal centro de treinamentos oficial
+              Microsoft para Microsoft Dynamics 365
+            </p>
+          </div>
+        </figure>
+
+        <InfoEmpresa/>
+        <Contato/>
+
+      </main>
+
     </div>
   );
 }
