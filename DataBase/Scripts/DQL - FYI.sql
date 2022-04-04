05a8e1747901cdaeb4a14a4d5938ee6c543ee695
Matheus O.G. do Nascimento <82417324+Matheus-O-G-do-Nascimento@users.noreply.github.com>
2022-04-04 10:16:05 -0300

    Delete DataBase directory

--- DataBase/Scripts/DQL - FYI.sql
+++
@@ -1,7 +0,0 @@
-	SELECT * FROM tipoUsuario
-	SELECT * FROM usuario
-	SELECT * FROM categoria
-	SELECT * FROM curso
-	SELECT * FROM turma
-	SELECT * FROM inscricao
-	SELECT * FROM inscricao WHERE idTurma = 3
