cef5f78deb3827dcdb645b6cec5d47de142b2e94
Matheus O.G. do Nascimento <82417324+Matheus-O-G-do-Nascimento@users.noreply.github.com>
2022-04-04 10:15:53 -0300

    Add files via upload

---
+++ DataBase/Scripts/DQL - FYI.sql
@@ -0,0 +1,7 @@
+	SELECT * FROM tipoUsuario
+	SELECT * FROM usuario
+	SELECT * FROM categoria
+	SELECT * FROM curso
+	SELECT * FROM turma
+	SELECT * FROM inscricao
+	SELECT * FROM inscricao WHERE idTurma = 3
