9bf8837cc596660616822678226325958910cab0
Fernando Santos Pereira <42944379879@info.scs>
2022-04-19 11:08:05 -0300

    Contato finalizado

--- src/Pages/Contato/index.js
+++ src/Pages/Contato/index.js
@@ -5,14 +5,10 @@ import IconTelefone from "../../Images/Contato/IconTelefone.svg";
 import Header from "../../Components/Header/Header.jsx";
 import Foooter from "../../Components/Footer/Footer.jsx"
 
-
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
 
-
-
 const Contato = () => {
-
-    
+  
 
   return (
     <>
@@ -65,15 +61,15 @@ const Contato = () => {
 
             </form>
           </div>
-          
 
           <div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1828.6337891578544!2d-46.65964771469206!3d-23.558831498626542!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59cd06184701%3A0xc5432b0e1fbb652f!2sAv.%20Paulista%2C%202006%20-%20406%20407%20-%20Bela%20Vista%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001310-200!5e0!3m2!1spt-BR!2sbr!4v1649852568387!5m2!1spt-BR!2sbr" width="700" height="500" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe class= 'map' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1828.6337821805823!2d-46.658931!3d-23.558832!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59cd102004fb%3A0x18040d33d46a99aa!2sAv.%20Paulista%2C%202006%20-%20Bela%20Vista%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001310-200!5e0!3m2!1spt-BR!2sbr!4v1650376776092!5m2!1spt-BR!2sbr"></iframe>
           </div>
         </div>
 
       </div>
 
+       
       <Foooter />
 
 
