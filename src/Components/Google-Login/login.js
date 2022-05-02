c7fc7177cbec33629af263b6c51043fcf6e0acf2
Lucas Reis Diniz <47435664871@info.scs>
2022-05-02 10:33:26 -0200

    Trazer informações do Google

--- src/Components/Google-Login/login.js
+++ src/Components/Google-Login/login.js
@@ -1,42 +1,42 @@
-import { GoogleLogin } from 'react-google-login'
-import Header from '../Header/Header'
+import React, { useState } from "react";
+import GoogleLogin from "react-google-login";
 
-import React from 'react'
+function App() {
+	const [name, setName] = useState();
+	const [email, setEmail] = useState();
+	const [profilePic, setProfilePic] = useState();
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-const clientId = "668949015501-b432fsitmd7ifcsfviigf3mudblhr1on.apps.googleusercontent.com"
-
-
-function LoginButton() {
-
-    const onSuccess = (res) => {
-        console.log("Login Success! Current user: ", res.profileObj)
-    }
-
-    const onFailure = (res) => {
-        console.log("Login Failed! res: ", res)
-    }
-
-    var profileObj = profileObj;
-
-    var image_profile = profileObj.imageURL;
-     
-    if (GoogleLogin.usuariocheck == true) {
-        
-    }
-    
-
-    return (
-        <div id="signInButton">
-            <GoogleLogin
-                clientId= {clientId}
-                onSuccess={onSuccess}
-                onFailure={onFailure}
-                cookiePolicy={'single_host_origin'}
-                isSignedIn={true}
-                usuariocheck={true}
-            />
-        </div>
-    )
+	const responseGoogle = (response) => {
+		console.log(response);
+		const {
+			profileObj: { name, email, imageUrl },
+		} = response;
+		setName(name);
+		setEmail(email);
+		setProfilePic(imageUrl);
+		setIsLoggedIn(true);
+	};
+	return (
+		<div className="container">
+			<GoogleLogin
+				clientId="668949015501-b432fsitmd7ifcsfviigf3mudblhr1on.apps.googleusercontent.com"
+				buttonText="Continuar com o Google"
+				onSuccess={responseGoogle}
+				onFailure={responseGoogle}
+			/>
+			{isLoggedIn ? (
+				<div style={{ textAlign: "center" }}>
+					<h1>User Information</h1>
+					<img className="profile" src={profilePic} alt="Profile" />
+					<p>Name: {name}</p>
+					<p>Email: {email}</p>
+				</div>
+			) : (
+				""
+			)}
+		</div>
+	);
 }
-export default LoginButton
 
+export default App;
